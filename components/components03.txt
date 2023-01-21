7ae1640d6250016fa2959866a3c1f2c019e5c689
dizars1776 <dizars1776@yahoo.gr>
2023-01-21 22:52:17 +0200

    add component-03 App.js

---
+++ components/components03.txt
@@ -0,0 +1,1 @@
+Write a class component called App that renders the Hello component within a div tag.
---
+++ components/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Hello } from "./Hello";
+
+export class App extends React.Component {
+  render() {
+    return <div><Hello /></div>
+  }
+}
