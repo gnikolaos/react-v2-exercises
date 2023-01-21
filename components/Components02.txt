63a147a8a6c167849f0d31d98535b385bc131308
dizars1776 <dizars1776@yahoo.gr>
2023-01-21 22:41:23 +0200

    add exercise components-02

---
+++ components/Components02.txt
@@ -0,0 +1,1 @@
+Write a Hello class components that renders the "Hello, World!" message within an h1 tag.
---
+++ components/src/index.js
@@ -0,0 +1,6 @@
+import {createRoot} from "react-dom/client"
+import { Hello } from "./Hello"
+
+const root = createRoot(document.querySelector("#root"))
+
+root.render(<Hello />)
