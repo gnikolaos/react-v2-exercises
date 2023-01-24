629587b9c4a2cdfbcef88c32d1021dcefb4a21ee
dizars1776 <dizars1776@yahoo.gr>
2023-01-24 12:07:42 +0200

    add cr05 exercise

---
+++ props/cr05.txt
@@ -0,0 +1,1 @@
+Modify the Age component so that the "Your age is " message is rendered only if the age prop is greater than 18. Otherwise render the "You are very young!" message.
--- props/src/Age.js
+++ props/src/Age.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 export class Age extends React.Component {
   render() {
-    return <p>Your age is {this.props.age}.</p>
+    return (
+      <div>
+        {this.props.age > 18 ? (
+          <p>Your age is {this.props.age}</p>
+        ) : (
+          <p>You are very young!</p>
+        )}
+      </div>
+    )
   }
 }
 
