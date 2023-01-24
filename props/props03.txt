739c541224ad6fe63cec9155e03849dc7dd722cc
dizars1776 <dizars1776@yahoo.gr>
2023-01-24 11:22:02 +0200

    add props03 exercise

---
+++ props/props03.txt
@@ -0,0 +1,1 @@
+Modify the Welcome component so that it receives a second prop called age and renders it below the 'welcome' message, within a p tag after the message "Your age is ".
--- props/src/Welcome.js
+++ props/src/Welcome.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 export class Welcome extends React.Component {
   render() {
-    return <p>Welcome, {this.props.name}!</p>
+    return (
+      <div>
+        <p>Welcome, {this.props.name}!</p>
+        <p>Your age is {this.props.age}.</p>
+      </div>
+    )
   }
 }
 
 Welcome.defaultProps = {
-  name: "Garen"
+  name: "Garen",
+  age: 30
 }
