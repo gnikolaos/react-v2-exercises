b060e93f4dbceba07dd93ee80f518bd2332df241
dizars1776 <dizars1776@yahoo.gr>
2023-01-24 11:59:34 +0200

    add cr04 exercise

--- props/src/Welcome.js
+++ props/src/Welcome.js
@@ -6,7 +6,9 @@ export class Welcome extends React.Component {
     return (
       <div>
         <p>Welcome, {this.props.name}!</p>
-        {(this.props.age > 18 && this.props.age < 65) && <Age age={this.props.age} />}
+        {this.props.age > 18 &&
+          this.props.age < 65 &&
+          this.props.name === 'John' && <Age age={this.props.age} />}
       </div>
     )
   }
