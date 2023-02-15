9b94cc8857e2da463a3326720b9111850adfba7a
Nikolaos Georgiou <dizars1776@yahoo.gr>
2023-02-15 19:24:29 +0200

    add stylingComps01 exercise

--- props/src/Welcome.js
+++ props/src/Welcome.js
@@ -4,7 +4,7 @@ import { Age } from './Age'
 export class Welcome extends React.Component {
   render() {
     return (
-      <div>
+      <div className='welcome'>
         <p>Welcome, {this.props.name}!</p>
         {this.props.age > 18 &&
           this.props.age < 65 &&
---
+++ props/stylingComps01.txt
@@ -0,0 +1,1 @@
+Create an index.css file and import it within the index.js file. Add a .welcome class to the Welcome component that changes its background color and adds a border.
