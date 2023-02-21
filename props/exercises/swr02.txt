d1c092fd0adc6f8253476089c84fc7fb1520acc5
Nikolaos Georgiou <dizars1776@yahoo.gr>
2023-02-21 21:39:27 +0200

    add swr02 exercise

---
+++ props/exercises/swr02.txt
@@ -0,0 +1,1 @@
+Modify the useGithubUser hook so that, if the username is null, no request is made.
--- props/src/router/useGithubUser2.js
+++ props/src/router/useGithubUser2.js
@@ -3,9 +3,9 @@ import useSWR from 'swr'
 const fetcher = url => fetch(url).then(response => response.json())
 
 function useGithubUser(username) {
- const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher)
-  
-  return { 
+ const { data, error } = useSWR((username !== null) ? `https://api.github.com/users/${username}` : null, fetcher)
+
+ return { 
     users: data,
     error,
     isLoading: !data && !error
